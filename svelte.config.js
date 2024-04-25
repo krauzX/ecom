70c2ffb9a627eca4374f8b369899eaea8cff769d
krauzX <aryanrajapandey@gmail.com>
2024-04-26 03:13:17 +0530

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,29 @@
+// import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: [
+		preprocess({
+			postcss: true,
+			preserve: ['ld+json']
+		})
+	],
+	kit: {
+		alias: {
+			'lib/*': 'src/lib/*'
+		},
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false
+		}
+	}
+	// vitePlugin: {
+	// 	experimental: {
+	// 		inspector: true
+	// 	}
+	// }
+}
+
+export default config
